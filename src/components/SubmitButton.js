bb9dd38a0495d51dd0f3d163a69be9bb90b5f21b
unknown <gerardstevan0406@gmail.com>
2023-09-18 23:11:39 +0800

    Changed the Form to  work for the previously commited function. Forgot to commit just now

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -8,6 +9,7 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
 import "../style/SubmitButton.css";
+import {postQuestion} from "./LocalStorageHandler.js"
 
 const Categories = [
   { value: 'Algorithm', label: 'Algorithm' },
@@ -23,17 +25,20 @@ const Difficulty =[
 ]
 
 function SubmitButton() {
+  
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   return (
     <div className="wrapper_submit">
         <h1> Submit your questions here:</h1>
-        <form>
+        <form onSubmit={handleSubmit(postQuestion)}>
         <Grid container spacing={1}>
               <Grid xs={12} item>
-                <TextField sx={{ border: '2px solid white', bgcolor: "#ffff", input: { color: "black" }}} label="Title" name="Questions" placeholder="Enter your Title" variant="filled" fullWidth required />
+                <TextField sx={{ border: '2px solid white', bgcolor: "#ffff", input: { color: "black" }}} label="Title" name="Questions" placeholder="Enter your Title" variant="filled" fullWidth required {...register("title", { required: true })}/>
               </Grid>
               <Grid xs={12} item>
                 <TextField sx={{ border: '2px solid white', bgcolor: "#ffff", input: { color: "black" } }} label="Decription" name="Message" multiline
-                   placeholder="Enter Description" variant="filled" fullWidth />
+                   placeholder="Enter Description" variant="filled" fullWidth {...register("description", { required: true })}/>
               </Grid>
               <Grid xs={12} item>
                 <TextField sx={{ border: '2px solid white', bgcolor: "#FFFF",
@@ -41,7 +46,7 @@ function SubmitButton() {
                     color: 'black', // Change this to your desired text color
                     textAlign: 'left',
                   },}}
-                  select defaultValue="" label="Category" name="Category" placeholder="Algorithm" variant="filled" fullWidth required>
+                  select defaultValue="" label="Category" name="Category" placeholder="Algorithm" variant="filled" fullWidth required {...register("category", { required: true })}>
                   {Categories.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
                           {option.label}
@@ -54,7 +59,7 @@ function SubmitButton() {
                     color: 'black', // Change this to your desired text color
                     textAlign: 'left',
                   },}}
-                  select defaultValue="" label="Difficulty" name="Difficulty" placeholder="Medium" variant="filled" fullWidth required>
+                  select defaultValue="" label="Difficulty" name="Difficulty" placeholder="Medium" variant="filled" fullWidth required {...register("difficulty", { required: true })}>
                   {Difficulty.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
                           {option.label}
@@ -65,14 +70,6 @@ function SubmitButton() {
                 <Button type="submit" variant="contained" color="primary" fullWidth> Submit</Button>
               </Grid>
         </Grid >
-            {/* <label>
-                <p> Questions?</p>
-                <input type="text" name="question" />
-                <input type="text" name="question" />
-                <input type="text" name="question" />
-                <input type="text" name="question" />
-            </label>
-            <button type="submit">Submit</button> */}
         </form>
     </div>
     
