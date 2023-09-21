dfb6c1e7cdb14566d8ac34232aeb653380526984
Richard Kurniawan <richardkrich19@gmail.com>
2023-09-21 15:23:51 +0800

    ExpandCollapseForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SubmitButton from './components/SubmitButton';
+import QuestionList from './components/QuestionList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <SubmitButton />
+       <QuestionList />
       </header>
     </div>
   );
