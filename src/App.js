7027ba551c24fb4c7ddba17a63f08b35bff1ccce
Richard Kurniawan <richardkrich19@gmail.com>
2023-09-21 17:12:18 +0800

    PathRouting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import SubmitButton from './components/SubmitButton';
 import QuestionList from './components/QuestionList';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useNavigate,
+  Link,
+} from "react-router-dom";
+import QuestionPage from './pages/QuestionPage';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <SubmitButton />
-       <QuestionList />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<QuestionPage />} />
+          {/* <Route path="/UserProfile" element={<UserProfile />} /> */} /* if want to add extra page  just copy and change the path
+        </Routes>
+      </BrowserRouter>
       </header>
     </div>
   );
