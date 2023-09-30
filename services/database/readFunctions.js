c030fa32e7168372d279fee19654c9a4ea6f31c1
Carmen <carmenang12@gmail.com>
2023-09-30 23:42:08 +0800

    fix login and sign up

--- services/database/readFunctions.js
+++ services/database/readFunctions.js
@@ -1,10 +1,23 @@
 const firebaseAdmin = require('./firebase.js');
 const db = firebaseAdmin.firestore();
 
-async function getUser(username) {
+async function getUser(email) {
+    /** 
+    console.log('Username:', username);
     const usersRef = db.collection("users");
     const doc = await usersRef.doc(username).get();
     return doc;
+    */
+   console.log('what is going on')
+   console.log(email)
+    const usersRef = db.collection("users");
+    const querySnapshot = await usersRef.where("email", "==", email).get();
+  
+    if (querySnapshot.empty) {
+      return null; 
+    }
+    console.log(querySnapshot.docs[0])
+    return querySnapshot.docs[0];
 }
 
 async function checkUserExists(email, password) {
