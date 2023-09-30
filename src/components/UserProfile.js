ed3ed8b8cf907827382394b6ec8a68496a6a348b
Carmen <carmenang12@gmail.com>
2023-09-30 23:42:08 +0800

    fix login and sign up

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { auth } from '../firebase-config'; 
 
 function UserProfile() {
+  const [userData, setUserData] = useState(null);
 
-    
+  const fetchUserData = async () => {
+    try {
+      const user = auth.currentUser;
+
+      if (!user) {
+        console.error("No user logged in.");
+        return;
+      }
+      console.log('Username:', user.email);
+      const response = await axios.get(`http://localhost:3001/getUser`, {'email': user.email});
+      console.log(response.data)
+      
+      setUserData(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []); // Run only once on component mount
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {userData && (
+        <div>
+          <p>Username: {userData.username}</p>
+          {/* Display other user information here */}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default UserProfile;
