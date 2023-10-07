bc23887f4a6b418311122a82fdcf156e26b5a7de
Brandon-OS <hieronimusbrandon@gmail.com>
2023-10-07 15:45:04 +0800

    untested question service backend

--- services/database/router.js
+++ services/database/router.js
@@ -17,7 +17,7 @@ app.post("/checkUserExists", async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
         const exists = await read.checkUserExists(email, password);
-        console.log(exists)
+        console.log(exists);
         if (exists) {
             res.status(200).send({ userExists: true });
         } else {
@@ -30,9 +30,9 @@ app.post("/checkUserExists", async (req, res) => {
 
 app.post("/handleLogin", async (req, res) => {
     try {
-        const email = req.body.email; 
+        const email = req.body.email;
         const password = req.body.password;
-        const response = await write.handleLogin(email, password); 
+        const response = await write.handleLogin(email, password);
         res.send(response);
     } catch (error) {
         console.error(error);
@@ -48,14 +48,14 @@ app.post("/handleSignup", async (req, res) => {
     } catch (error) {
         console.error(error);
     }
-}); 
+});
 
 app.post("/delete", async (req, res) => {
     try {
         const username = req.body.username;
-        console.log(username)
+        console.log(username);
         await write.removeUser(username);
-        res.send('User removed');
+        res.send("User removed");
     } catch (error) {
         console.error(error);
     }
@@ -68,12 +68,18 @@ app.post("/add", async (req, res) => {
         const language = req.body.language;
         const level = req.body.level;
         const password = req.body.password;
-        const response = await write.addUser(username, email, password, language, level);
+        const response = await write.addUser(
+            username,
+            email,
+            password,
+            language,
+            level
+        );
         res.send(response);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 app.get("/get", async (req, res) => {
     try {
@@ -83,11 +89,11 @@ app.get("/get", async (req, res) => {
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 app.patch("/updateUser", async (req, res) => {
     try {
-        const username = req.body.username; 
+        const username = req.body.username;
         const language = req.body.language;
         const level = req.body.level;
         const response = await write.updateUser(username, language, level);
@@ -95,7 +101,54 @@ app.patch("/updateUser", async (req, res) => {
     } catch (error) {
         console.error(error);
     }
-})
+});
+
+app.post("/question", async (req, res) => {
+    try {
+        const title = req.body.title;
+        const category = req.body.category;
+        const difficulty = req.body.difficulty;
+        const description = req.body.description;
+        const response = await write.addQuestion(
+            title,
+            category,
+            difficulty,
+            description
+        );
+        res.status(200).send(response);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/question", async (req, res) => {
+    try {
+        const response = null;
+        if (req.title === undefined) {
+            response = await read.getAllQuestions();
+        } else {
+            const title = req.query.title;
+            response = await read.getQuestion(title);
+        }
+
+        if (response === null) {
+            res.status(404).send("Question not found");
+        }
+        res.status(200).send(response);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.delete("/question", async (req, res) => {
+    try {
+        const title = req.query.title;
+        await write.deleteQuestion(title);
+        res.status(200).send("Question deleted");
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
